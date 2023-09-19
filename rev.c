8fcaf2d7735892f13132bb0ea3c97f087345c08f
abdul1ah33 <140663753+abdul1ah33@users.noreply.github.com>
2023-09-20 01:55:10 +0300

    Add files via upload
    rev done

---
+++ rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_string - print a string
+ * Description: prints a string in reverse
+ * @arguments: a pointer to string
+ * @printed_counter: number of printed characters
+ * Return: number of words
+ */
+int reverse_string(va_list arguments, int printed_counter)
+{
+	char *string_buffer;
+	int length;
+
+	string_buffer = va_arg(arguments, char *);
+	length = _strlen(string_buffer);
+	printed_counter += length;
+	length--;
+
+	for (;length != -1; length--)
+	{
+		_putchar(string_buffer[length]);
+	}
+	return (printed_counter);
+}
